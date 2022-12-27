cf5502df94bbf538a4666fd8312334fc82ae2fdd
superballer365 <matthew.balnis@uconn.edu>
2022-12-26 19:54:53 -0500

    Installing react three fiber and setting up an empty scene

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      tabIndex={0}
+      style={{ width: "100%", height: "100%", backgroundColor: "black" }}
+    >
+      <Canvas camera={{ position: [0, 25, 0] }}>
+        <primitive object={new THREE.AxesHelper(10)} />
+        <primitive object={new THREE.GridHelper()} />
+        <color attach="background" args={["black"]} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
