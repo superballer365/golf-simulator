4816d1af57087c80825cf4e92da4bbec8e29113c
superballer365 <matthew.balnis@uconn.edu>
2022-12-27 21:38:33 -0500

    Adding basic ball physics and reset button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,87 @@
-import React from "react";
-import { useGLTF } from "@react-three/drei";
+import React, { SetStateAction } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import GolfBall from "./components/GolfBall";
 
+/**
+ * @param tSeconds number of seconds since start of simulation
+ * @param initialY initial y position
+ * @param initialVy initial y velocity
+ * @returns the current y position, in meters
+ */
+function yOt(tSeconds: number, initialY: number, initialVy: number) {
+  const yAccel = -9.82; // gravity, in m/s^2
+  return initialY + initialVy * tSeconds + 0.5 * yAccel * Math.pow(tSeconds, 2);
+}
+
+/**
+ * @param tSeconds number of seconds since start of simulation
+ * @param initialVy initial y velocity
+ * @returns the current y velocity, in meters
+ */
+function vyOt(tSeconds: number, initialVy: number) {
+  const yAccel = -9.82; // gravity, in m/s^2
+  return initialVy + yAccel * tSeconds;
+}
+
+const initialBallPosition: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+const initialBallVelocity: THREE.Vector3 = new THREE.Vector3(0, 10, 0);
+
 function App() {
+  const [ballPosition, setBallPosition] = React.useState(initialBallPosition);
+  const [currentTime, setCurrentTime] = React.useState(0);
+
+  const handleResetClick = () => {
+    setBallPosition(initialBallPosition);
+    setCurrentTime(0);
+  };
+
   return (
     <div
       tabIndex={0}
       style={{ width: "100%", height: "100%", backgroundColor: "black" }}
     >
-      <Canvas camera={{ position: [0, 5, 0] }}>
+      <button
+        style={{ position: "absolute", top: 10, left: 10, zIndex: 10 }}
+        onClick={handleResetClick}
+      >
+        Reset
+      </button>
+      <Canvas camera={{ position: [0, 0, 50] }}>
         <primitive object={new THREE.AxesHelper(10)} />
         <primitive object={new THREE.GridHelper()} />
-        <GolfBall />
+        <GolfBall position={ballPosition} />
+        <Ticker
+          currentTime={currentTime}
+          setBallPosition={setBallPosition}
+          setCurrentTime={setCurrentTime}
+        />
         <color attach="background" args={["black"]} />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
       </Canvas>
     </div>
   );
+}
+
+interface TickerProps {
+  currentTime: number;
+  setBallPosition: React.Dispatch<SetStateAction<THREE.Vector3>>;
+  setCurrentTime: React.Dispatch<SetStateAction<number>>;
+}
+
+function Ticker({ currentTime, setBallPosition, setCurrentTime }: TickerProps) {
+  useFrame((state, delta) => {
+    const newYPosition = yOt(
+      currentTime,
+      initialBallPosition.y,
+      initialBallVelocity.y
+    );
+    setBallPosition((prev) => new THREE.Vector3(prev.x, newYPosition, prev.z));
+    setCurrentTime((prev) => (prev += delta));
+  });
+
+  return null;
 }
 
 export default App;
--- client/src/components/GolfBall.tsx
+++ client/src/components/GolfBall.tsx
@@ -1,14 +1,20 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React from "react";
+import { Vector3 } from "three";
 
-export default function GolfBall() {
+interface GolfBallProps {
+  position: Vector3;
+}
+
+export default function GolfBall({ position }: GolfBallProps) {
+  const [initialPosition] = React.useState(position);
   const mesh = React.useRef<THREE.Mesh>(null!);
   const { scene } = useGLTF("models/golf_ball/scene.gltf");
 
-  useFrame(() => {
-    mesh.current.rotateX(0.01);
-  });
+  React.useEffect(() => {
+    mesh.current.position.set(position.x, position.y, position.z);
+  }, [position]);
 
-  return <primitive ref={mesh} object={scene} />;
+  return <primitive ref={mesh} object={scene} position={initialPosition} />;
 }
