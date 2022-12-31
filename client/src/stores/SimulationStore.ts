2817ed4ba6be05c5a98dae3d69020154f4162bea
superballer365 <matthew.balnis@uconn.edu>
2022-12-31 11:46:27 -0500

    Starting to create a store for handling simulation state

---
+++ client/src/stores/SimulationStore.ts
@@ -0,0 +1,67 @@
+import create from "zustand";
+import * as THREE from "three";
+
+export enum SimulationStatus {
+  NotStarted,
+  InProgress,
+  Complete,
+}
+
+export interface LaunchConditions {
+  /** Initial Speed, in meters per second */
+  speed: number;
+  /** Launch angle, in radians */
+  verticalAngle: number;
+}
+
+export interface SimulationState {
+  status: SimulationStatus;
+  ballPosition: THREE.Vector3;
+  launchConditions: LaunchConditions;
+  actions: {
+    start: () => void;
+    complete: () => void;
+    reset: () => void;
+    updateLaunchConditions: (newConditions: Partial<LaunchConditions>) => void;
+    updateBallPosition: (newPosition: THREE.Vector3) => void;
+  };
+}
+
+const useSimulationStore = create<SimulationState>((set) => ({
+  status: SimulationStatus.NotStarted,
+  ballPosition: new THREE.Vector3(0, 0, 0),
+  launchConditions: {
+    speed: 10,
+    verticalAngle: 0.5236, // 30 degrees
+  },
+  actions: {
+    start: () => set({ status: SimulationStatus.InProgress }),
+    complete: () => set({ status: SimulationStatus.Complete }),
+    reset: () =>
+      set({
+        status: SimulationStatus.NotStarted,
+        ballPosition: new THREE.Vector3(0, 0, 0),
+      }),
+    updateLaunchConditions: (newConditions: Partial<LaunchConditions>) =>
+      set((state) => {
+        if (state.status !== SimulationStatus.NotStarted) return state; // only update launch conditions when simulation has not started.
+        return {
+          launchConditions: { ...state.launchConditions, ...newConditions },
+        };
+      }),
+    updateBallPosition: (newPosition: THREE.Vector3) =>
+      set((state) => {
+        if (state.status !== SimulationStatus.InProgress) return state; // only update ball position while simulation is in progress.
+        return { ballPosition: newPosition };
+      }),
+  },
+}));
+
+export const useSimulationStatus = () =>
+  useSimulationStore((state) => state.status);
+export const useBallPosition = () =>
+  useSimulationStore((state) => state.ballPosition);
+export const useLaunchConditions = () =>
+  useSimulationStore((state) => state.launchConditions);
+export const useSimulationActions = () =>
+  useSimulationStore((state) => state.actions);
