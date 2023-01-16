b66066813688499c931b1eeda52eda051fd28655
superballer365 <matthew.balnis@uconn.edu>
2023-01-16 18:26:14 -0500

    Adding texture to grass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 import GolfBall from "./components/GolfBall";
 import { useSimulationActions } from "./stores/SimulationStore";
@@ -9,6 +9,7 @@ import Overlay from "./components/Overlay";
 import CarryTracker from "./components/CarryTracker";
 import SettingsControls from "./components/SettingsControls";
 import { OrbitControls, Sky } from "@react-three/drei";
+import { TextureLoader } from "three";
 
 export default function App() {
   const theme = useTheme();
@@ -25,14 +26,13 @@ export default function App() {
           topCenter={<CarryTracker />}
           topRight={<SettingsControls />}
         >
-          <Canvas camera={{ position: [0, 0, 50] }}>
+          <Canvas camera={{ position: [0, 3, 50] }}>
             {/* NOTE: we may need to introduce a context bridge at some point if we need to use
           information from the matinine context INSIDE of the canvas. We don't have this need
           just yet. See here for more info: 
           https://standard.ai/blog/introducing-standard-view-and-react-three-fiber-context-bridge/ */}
             <OrbitControls />
             <ambientLight />
-            <pointLight position={[10, 10, 10]} />
             <Sky
               distance={450000}
               sunPosition={[5, 1, 8]}
@@ -55,17 +55,24 @@ export default function App() {
 }
 
 function Ground() {
+  const texture = useLoader(TextureLoader, "textures/grasslight-big.jpeg");
+
+  if (texture) {
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1500, 1500);
+    texture.anisotropy = 16;
+  }
+
   return (
-    // The plane is rotated by 90 degrees along the X-axis
-    // This is because, by default, planes are rendered
-    // in the X-Y plane, where Y is the up direction
-    <mesh
-      position={[0, -1, 0]}
-      rotation={[Math.PI / 2, 0, 0]}
-      scale={[1000, 1000, 1000]}
-    >
-      <planeBufferGeometry />
-      <meshBasicMaterial color="green" side={THREE.DoubleSide} />
+    <mesh receiveShadow position={[0, -1, 0]} rotation={[Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[10000, 10000]} />
+      {texture && (
+        <meshPhongMaterial
+          attach="material"
+          map={texture}
+          side={THREE.DoubleSide}
+        />
+      )}
     </mesh>
   );
 }
