fc01783a8e4b0d9b5a77dd5d0a67072229b8bdf9
superballer365 <matthew.balnis@uconn.edu>
2023-01-16 17:43:46 -0500

    Adding a basic skybox and ground plane

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import LaunchControls from "./components/LaunchControls";
 import Overlay from "./components/Overlay";
 import CarryTracker from "./components/CarryTracker";
 import SettingsControls from "./components/SettingsControls";
+import { OrbitControls, Sky } from "@react-three/drei";
 
 export default function App() {
   const theme = useTheme();
@@ -29,20 +30,43 @@ export default function App() {
           information from the matinine context INSIDE of the canvas. We don't have this need
           just yet. See here for more info: 
           https://standard.ai/blog/introducing-standard-view-and-react-three-fiber-context-bridge/ */}
-            <primitive object={new THREE.AxesHelper(10)} />
+            <OrbitControls />
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Sky
+              distance={450000}
+              sunPosition={[5, 1, 8]}
+              inclination={0}
+              azimuth={0.25}
+            />
+            <Ground />
+            <GolfBall />
+            <PhysicsTicker />
+            {/* <primitive object={new THREE.AxesHelper(10)} />
             <primitive
               object={new THREE.GridHelper(100)}
               rotation={[Math.PI / 2, 0, 0]}
-            />
-            <GolfBall />
-            <PhysicsTicker />
-            <color attach="background" args={["black"]} />
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
+            /> */}
           </Canvas>
         </Overlay>
       </Box>
     </MantineProvider>
+  );
+}
+
+function Ground() {
+  return (
+    // The plane is rotated by 90 degrees along the X-axis
+    // This is because, by default, planes are rendered
+    // in the X-Y plane, where Y is the up direction
+    <mesh
+      position={[0, -1, 0]}
+      rotation={[Math.PI / 2, 0, 0]}
+      scale={[1000, 1000, 1000]}
+    >
+      <planeBufferGeometry />
+      <meshBasicMaterial color="green" side={THREE.DoubleSide} />
+    </mesh>
   );
 }
 
